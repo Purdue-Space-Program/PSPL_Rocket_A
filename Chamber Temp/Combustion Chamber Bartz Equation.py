d8aaf6f61ca32a145484575698137cbd42f50941
mukunds18 <mukunds@purdue.edu>
2025-10-05 14:37:54 -0400

    Update Combustion Chamber Bartz Equation.py

--- Chamber Temp/Combustion Chamber Bartz Equation.py
+++ Chamber Temp/Combustion Chamber Bartz Equation.py
@@ -1,19 +1,19 @@
 from math import exp, erfc
 
 def heat_transfer_coefficient():
-    Dt = 1 #diameter of engine throat
-    mu = 1 #dynamic viscosity of the combustion gas
-    Cp = 1 #specific heat of the combustion gas at constant pressure
-    P0 = 1 #stagnation/chamber pressure (I think)
+    Dt = 0.0498 #diameter of engine throat
+    mu = 4e-5 #dynamic viscosity of the combustion gas (Pa*s)
+    Cp = 2500 #specific heat of the combustion gas at constant pressure(J/kg*K)
+    P0 = 3e6 #stagnation/chamber pressure (I think) (Pa)
     g = 9.81 #gravitational constant
-    Rt = 1 #radius of the throat curve
+    Rt = 0.05 #radius of the throat curve
     Area_ratio = 1 #ratio of throat area to the local area at the point of interest
-    gamma = 1 #specific heat ratio
-    c_star = 1 #characteristic exhaust velocity
-    Pr = 1 #Prandtl number of the combustion gas
-    Twg = 1 #wall temperature (K)
-    T0 = 1 #stagnation temperature (K)
-    M = 1 #Mach number
+    gamma = 1.2 #specific heat ratio, typical value for hot combustion gases
+    c_star = 1248 #characteristic exhaust velocity
+    Pr = 0.7 #Prandtl number of the combustion gas
+    Twg = 800 #wall temperature (K) because steel can withstand up to 1100 K but safety margin
+    T0 = 3500 #stagnation temperature (K)
+    M = 1 #Mach number at the throat
 
     #The sigma term of the Bartz equation split into different terms
     sigma_parentheses1 = ((0.5 * (Twg / T0) * (1 + ((gamma - 1)/2) * (M**2))) + 0.5) ** 0.68
@@ -30,64 +30,51 @@ def heat_transfer_coefficient():
 
     return bartz_equation
 
-def temperature_surface_calculation(heat_transfer_coefficient):
+def temperature_surface_calculation(heat_transfer_coefficient_value):
 
-    alpha = 1 #thermal diffusivity of the chamber wall material
-    T_infinity = 1 #combustion temperature (Kelvin)
+    alpha = 4e-6 #thermal diffusivity of the chamber wall material
+    T_infinity = 3500 #combustion temperature (Kelvin)
     T_initial = 293 #room temperature in Kelvin
     t = 10 #burn time in seconds
-    k = 1 #thermal conductivity of the chamber wall material
+    k = 16 #thermal conductivity of the chamber wall material (W/mK)
 
     #Surface temperature equation split into different terms
-    Surface_temp_term1 = (-1) * exp(((heat_transfer_coefficient ** 2) * alpha * t) / (k**2))
-    Surface_temp_term2 = erfc((heat_transfer_coefficient * ((alpha * t) ** 0.5)) / k)
+    Surface_temp_term0 = (heat_transfer_coefficient_value ** 2) * alpha * t
+    Surface_temp_term1 = (-1) * exp((Surface_temp_term0) / (k**2))
+    Surface_temp_term2 = erfc((heat_transfer_coefficient_value * ((alpha * t) ** 0.5)) / k)
     Surface_temp_term3 = T_infinity - T_initial 
 
     surface_temp = (Surface_temp_term1 * Surface_temp_term2 * Surface_temp_term3) + T_initial
 
     return surface_temp
 
+'''
+def minimum_wall_thickness():
+    #using the formula (PD)/(2(SE+PY))
+    P = 0.000435113 #chamber pressure (psi)
+    D = 0.0498 #outer diameter of engine throat
+    S = #allowed stress value which depends on the specific grade of stainless steel and design temp
+    E = 1 #longitudinal weld join quality factor, depdends if material has been radiographed
+    Y = 0.7 #wall thickness coefficient, stainless steel for above 900F
 
-def main():
-    heat_transfer_coefficient = heat_transfer_coefficient()
-    print("Heat flux onto the chamber wall:", heat_transfer_coefficient)
-
-    temperature_surface = temperature_surface_calculation(heat_transfer_coefficient)
-    print("Surface temperature of the chamber wall:", temperature_surface)
-
-
-
-
-
-
-
-
-
-    #now that we have the heat transfer, we can calculate the heat flux 
-    #I think this is done by dividing bartz equation by the fluid's specific heat multiplied by the temperature difference between the wall and the gas
+    thickness = (P * D) / (2 * ((S * E) + (P * Y)))
+    return thickness
 '''
 
-    gamma = 1 #specific heat of the fluid
-    Tw = 1 #temperature of the wall
-    Tg = 1 #temperature of the gas
+def main():
+    heat_transfer_coefficient_value = heat_transfer_coefficient()
+    print("Heat flux (h) onto the chamber wall:", heat_transfer_coefficient_value)
 
-    heat_flux =  heat_transfer_coefficient / (gamma * (Tw - Tg))
-    print("Heat flux onto the chamber wall", heat_flux)
-
-    surface_area = 1 #surface area of the chamber wall
-    final_heatTransfer = heat_flux * surface_area
-    print("final heat transfer onto the chamber wall", final_heatTransfer)
-
-    #now to find the maximum wall temperature
-    burn_time = 10 #burn time (seconds)
-    wall_density = 1 #kg/(m^3)
-    wall_specific_heat = 1 #specific heat of the wall material (J/(kg*K))
-    wall_thickness = 1 #thickness of the wall (m)
-
-    wall_mass = surface_area * wall_thickness * wall_density
-    delta_T = final_heatTransfer * burn_time / (wall_mass * wall_specific_heat)
+    temperature_surface = temperature_surface_calculation(heat_transfer_coefficient_value)
+    print("Surface temperature of the chamber wall (in Kelvin):", temperature_surface)
 
     '''
+    thickness = minimum_wall_thickness()
+    print("Minimum wall thickness of the chamber (in units idk yet):", thickness)
+    '''
+
+if __name__ == "__main__":
+    main()
 
 
 
