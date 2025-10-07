7bdb0540813f397053418ec22432a08ac48c9653
Daksh-Gupta <59689911+Daksh-Gupta@users.noreply.github.com>
2025-10-07 16:03:28 -0400

    Update main.py

--- Injector orifice sizing/main.py
+++ Injector orifice sizing/main.py
@@ -11,18 +11,10 @@ standard_bits_inch = {
     "1/64": 1/64, "1/32": 1/32, "3/64": 3/64, "1/16": 1/16, "5/64": 5/64,
     "3/32": 3/32, "7/64": 7/32, "1/8": 1/8, "9/64": 9/64, "5/32": 5/32,
     "11/64": 11/64, "3/16": 3/16, "13/64": 13/64, "7/32": 7/32, "15/64": 15/64,
-    "1/4": 1/4, "5/16": 5/16, "3/8": 3/8, "7/16": 7/16, "1/2": 1/2,
 
     # Number drill sizes (#80–#1, partial list)
     "#80": 0.0135, "#70": 0.0280, "#60": 0.0400, "#50": 0.0700,
     "#40": 0.0980, "#30": 0.1285, "#20": 0.1610, "#10": 0.1935, "#1": 0.2280,
-
-    # Letter drill sizes (A–Z, partial list)
-    "A": 0.234, "B": 0.238, "C": 0.242, "D": 0.246, "E": 0.250,
-    "F": 0.257, "G": 0.261, "H": 0.266, "I": 0.272, "J": 0.277,
-    "K": 0.281, "L": 0.290, "M": 0.295, "N": 0.302, "O": 0.316,
-    "P": 0.323, "Q": 0.332, "R": 0.339, "S": 0.348, "T": 0.358,
-    "U": 0.368, "V": 0.377, "W": 0.386, "X": 0.397, "Y": 0.404, "Z": 0.413,
 }
 
 #Inputs
@@ -48,12 +40,13 @@ D_s = D_c / 5 # Diameter of pintle shaft
 R_s = D_s / 2 # Radius of pintle shaft
 skip_dist = D_s # This means that the skip distance ratio = 1. This is a good rule of thumb.
 total_area_orifice_lox = m_dot_lox / (C_D_lox * np.sqrt(2 * rho_lox * pressure_drop))
+area_orifice_ipa = m_dot_ipa / (C_D_ipa * np.sqrt(2 * rho_ipa * pressure_drop))
 
 #Constants and initializiing variables
 g = 9.81 # [m/s^2]
 N_rows = 1
 annular_thickness = 0 # [m]
-area_orifice_ipa = 0 # [m^2]
+#area_orifice_ipa = 0 # [m^2]
 area_orifice_lox = 0 # [m^2]
 D_lox_orifice = 0 # [m]
 velocity_ipa = 0 # [m/s]
@@ -82,14 +75,13 @@ for N_lox in range(N_lox_min, N_lox_max, 2):
     closest_bit, min_distance = closest_bit_size(D_lox_orifice * M2IN)
     D_lox_orifice_real = closest_bit[1] * IN2M
     area_orifice_lox = total_area_orifice_lox / N_lox
-    annular_thickness = np.pi * rho_lox * D_lox_orifice_real / (4 * rho_ipa * of_ratio**2)
+    #annular_thickness = np.pi * rho_lox * D_lox_orifice_real / (4 * rho_ipa * of_ratio**2)
     #area_orifice_ipa = np.pi * (R_s + annular_thickness)**2 - np.pi * R_s**2
-    area_orifice_ipa = m_dot_ipa / (C_D_ipa * np.sqrt(2 * rho_ipa * pressure_drop))
     velocity_lox = m_dot_lox / (rho_lox * area_orifice_lox)
     #velocity_ipa = C_D_ipa * np.sqrt(2 * pressure_drop / rho_ipa)
     #area_orifice_ipa = m_dot_ipa / (rho_lox * velocity_ipa)
     velocity_ipa = m_dot_ipa / (rho_ipa * area_orifice_ipa)
-    #annular_thickness = np.sqrt((area_orifice_ipa + np.pi * R_s**2) / np.pi) - R_s
+    annular_thickness = np.sqrt((area_orifice_ipa + np.pi * R_s**2) / np.pi) - R_s
     tmr_real = (m_dot_lox * velocity_lox) / (m_dot_ipa * velocity_ipa)
     bf = N_lox * D_lox_orifice_real / (np.pi * D_s)
     if bf > 1:
