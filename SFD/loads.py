d2d1c7c868d4694e3a51e348fde9d8f628df74d6
GaryHuang124 <garyhuang124@gmail.com>
2025-11-02 13:44:52 -0500

    Tried fixing graphs and using rotational acceleration for shear and bending (shear bending graph still not valid)

--- SFD/loads.py
+++ SFD/loads.py
@@ -62,21 +62,25 @@ bending_array = np.array(sfd.calcBending(shear_array, length_along_rocket_linspa
 axial_array = np.array(sfd.calcAxial(thrust, ax, linear_density_array, length_along_rocket_linspace))
 
 
-variable = "axial_array"
+variable = "shear_array"
 N2LBS = 0.224809
 M2FT = 3.28084
 
 if variable == "shear_array":
     plot = shear_array * N2LBS
     ylabel = "Shear Force [lbs]"
+    title = "Shear Forces"
 if variable == "bending_array":
     plot = bending_array * N2LBS
     ylabel = "Bending Moment [lbs-ft]"
+    title = "Bending Moments"
 if variable == "axial_array":
     plot = axial_array * N2LBS
     ylabel = "Axial Force [lbs]"
+    title = "Axial Forces"
 
 plt.plot(length_along_rocket_linspace * M2FT, plot)
+plt.title(title)
 plt.xlabel("Length from aft [ft]")
 plt.ylabel(ylabel)
 plt.show()
