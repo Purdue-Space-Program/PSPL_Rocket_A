becb3f6029728d1b4c1eba9783673a3505b13b16
monkeybank <56950327+monkeybank@users.noreply.github.com>
2025-11-02 21:07:59 -0500

    Implemented everything into main and observed basic functionality

--- Pressure drop/main.py
+++ Pressure drop/main.py
@@ -176,52 +176,17 @@ def sharp_expansion_ipa(outer_dia_one, outer_dia_two, wall_thic, abs_roughness):
     drop = core.dP_from_K(K, rho=density_ipa.to(u.kilogram / u.meter**3).magnitude, V=line_vel_ipa.to(u.meter / u.second).magnitude) * u.pascal
     drop = drop.to(u.psi)
     return drop
-
-'''
-def sharp_edged_inlet():
-    K = 0.5
-    return K
-
-def inward_projecting_sharp_edged_inlet():
-    K = 0.78
-    return K
-
-def sharp_edged_inlet_at_angle(angle):
-    K = 0.5 + 0.3 * math.cos(angle) + 0.2 * math.cos(angle)**2
-    return K
-
-def rounded_inlet(radius, diameter):
-    r_by_d = radius / diameter
-    if r_by_d > 0.2:
-        K = 0.03
-    else:
-        K = 0.5 - 1.07 * r_by_d**(0.5) - 2.13 * r_by_d + 8.24 * r_by_d**(1.5) - 8.48 * r_by_d**2 + 2.90 * r_by_d**(2.5)
-    return K
-
-def sharp_edged_sudden_contraction(diameter_one, diameter_two):
-    K = 0.5 * (1 - (diameter_one / diameter_two)**2)
-    return K
-
-def sharp_edged_sudden_expansion(diameter_one, diameter_two):
-    K = 1.05 * (1 - (diameter_one / diameter_two)**2)**2
-    return K
-
-def outlet():
-    K = 1.05
-    return K
-
-def bend(angle, radius, diameter, friction_factor):
-    r_by_d = radius / diameter
-    K = friction_factor * angle * r_by_d + (0.1 + 2.4 * friction_factor) * math.sin(angle / 2) + (6.6 * friction_factor * math.sin(angle / 2)**0.5 + math.sin(angle / 2)) / (r_by_d**((4 * angle) / math.pi))
-    return K
-
-def valve(flow_coefficient, diameter):
-    K = (890.4 * diameter**4) / (flow_coefficient**2)
-    return K
-'''
     
 def main():
     # Majority of this code is by Keshav Narayanan and Isaiah Jarvis from Rocket 4
+    chamber_press = 150 * u.psi  # Pressure in psi
+    chamber_press = chamber_press.to(u.pascal)  # Convert to pascals
+
+    injector_inlet_press = chamber_press * 1.2  # Assumes the injector inlet pressure is 20% higher than the chamber pressure
+
+    current_press_ipa = injector_inlet_press    # Set to injector inlet pressure because this is the pressure right before entering injector
+    current_press_ox = injector_inlet_press     # Works backwards from the chamber, so this is where we will start. All pressure drops will be added to this number
+
     reading_ox = False    #The excel sheet will assume it is reading Fuel until the 'Fu' keyword comes up. Then it will switch to Oxygen.
 
     file = pd.read_excel('pressure_drop_components.xlsx')
@@ -236,80 +201,73 @@ def main():
             print("\nLOx pressure drops:\n")
 
         if file.loc[row, 'Type'] == 'Straight Tube':
-
-            '''
             length = file.loc[row, 'Property 1'] * u.inch
             d_outer = file.loc[row, 'Property 3'] * u.inch
             wall_thickness = file.loc[row, 'Property 4'] * u.inch
             abs_rough = file.loc[row, 'Property 5'] * u.mm
-            '''
 
             if reading_ox == True:
-                #part_pd = straight_dp_ox(length, d_outer, wall_thickness, abs_rough)
-                print(f"{name}\nPart Type: Straight Tube\nPressure Drop: {1:.2f}\n")
-                #current_press_ox += part_pd
+                part_pd = straight_tube_ox(length, d_outer, wall_thickness, abs_rough)
+                print(f"{name}\nPart Type: Straight Tube\nPressure Drop: {part_pd:.2f}\n")
+                current_press_ox += part_pd
             else:
-                #part_pd = straight_dp_eth(length, d_outer, wall_thickness, abs_rough)
-                print(f"{name}\nPart Type: Straight Tube\nPressure Drop: {1:.2f}\n")
-                #current_press_eth += part_pd
+                part_pd = straight_tube_ipa(length, d_outer, wall_thickness, abs_rough)
+                print(f"{name}\nPart Type: Straight Tube\nPressure Drop: {part_pd:.2f}\n")
+                current_press_ipa += part_pd
 
         if file.loc[row, 'Type'] == 'Bend':
-
-            '''angle = file.loc[row, 'Property 1'] * u.degree
+            angle = file.loc[row, 'Property 1'] * u.degree
             bend_radius = file.loc[row, 'Property 2'] * u.inch
             d_outer = file.loc[row, 'Property 3'] * u.inch
             wall_thickness = file.loc[row, 'Property 4'] * u.inch
-            abs_rough = file.loc[row, 'Property 5'] * u.mm'''
+            abs_rough = file.loc[row, 'Property 5'] * u.mm
 
             if reading_ox == True:
-                #part_pd = bend_dp_ox(angle, bend_radius, d_outer, wall_thickness, abs_rough)
-                print(f"{name}\nPart Type: Bend\nPressure Drop: {1:.2f}\n")
-                #current_press_ox += part_pd
+                part_pd = bend_ox(angle, bend_radius, d_outer, wall_thickness, abs_rough)
+                print(f"{name}\nPart Type: Bend\nPressure Drop: {part_pd:.2f}\n")
+                current_press_ox += part_pd
             else:
-                #part_pd = bend_dp_eth(angle, bend_radius, d_outer, wall_thickness, abs_rough)
-                print(f"{name}\nPart Type: Bend\nPressure Drop: {1:.2f}\n")
-                #current_press_eth += part_pd
+                part_pd = bend_ipa(angle, bend_radius, d_outer, wall_thickness, abs_rough)
+                print(f"{name}\nPart Type: Bend\nPressure Drop: {part_pd:.2f}\n")
+                current_press_ipa += part_pd
 
 
         if file.loc[row, 'Type'] == 'Ball Valve':
-
-            '''inner_dia = file.loc[row, 'Property 1'] * u.inch
+            inner_dia = file.loc[row, 'Property 1'] * u.inch
             Cv = file.loc[row, 'Property 2']
             d_outer = file.loc[row, 'Property 3'] * u.inch
             wall_thickness = file.loc[row, 'Property 4'] * u.inch
-            abs_rough = file.loc[row, 'Property 5'] * u.mm'''
+            abs_rough = file.loc[row, 'Property 5'] * u.mm
 
             if reading_ox == True:
-                #part_pd = ball_valve_dp_ox(Cv, inner_dia, wall_thickness, abs_rough)
-                print(f"{name}\nPart Type: Ball Valve\nPressure Drop: {1:.2f}\n")
-                #current_press_ox += part_pd
+                part_pd = ball_valve_ox(Cv, inner_dia, wall_thickness, abs_rough)
+                print(f"{name}\nPart Type: Ball Valve\nPressure Drop: {part_pd:.2f}\n")
+                current_press_ox += part_pd
             else:
-                #part_pd = ball_valve_dp_eth(Cv, inner_dia, wall_thickness, abs_rough)
-                print(f"{name}\nPart Type: Ball Valve\nPressure Drop: {1:.2f}\n")
-                #current_press_eth += part_pd
+                part_pd = ball_valve_ipa(Cv, inner_dia, wall_thickness, abs_rough)
+                print(f"{name}\nPart Type: Ball Valve\nPressure Drop: {part_pd:.2f}\n")
+                current_press_ipa += part_pd
 
         if file.loc[row, 'Type'] == 'Venturi':
             if reading_ox == True:
-                #venturi_inlet_press = current_press_ox / .8    #assumes an 80% pressure recovery rate
-                #venturi_pd = venturi_inlet_press - current_press_ox
-                print(f"{name}\nPart Type: Venturi\nPressure Drop: {1:.2f}\n")
-                #current_press_ox += venturi_pd
+                venturi_inlet_press = current_press_ox / .8    #assumes an 80% pressure recovery rate
+                venturi_pd = venturi_inlet_press - current_press_ox
+                print(f"{name}\nPart Type: Venturi\nPressure Drop: {venturi_pd:.2f}\n")
+                current_press_ox += venturi_pd
             else:
-                #venturi_inlet_press = current_press_eth / .8
-                #venturi_pd = venturi_inlet_press - current_press_eth
-                print(f"{name}\nPart Type: Venturi\nPressure Drop: {1:.2f}\n")
-                #current_press_eth += venturi_pd
+                venturi_inlet_press = current_press_ipa / .8
+                venturi_pd = venturi_inlet_press - current_press_ipa
+                print(f"{name}\nPart Type: Venturi\nPressure Drop: {venturi_pd:.2f}\n")
+                current_press_ipa += venturi_pd
 
             
 
 
     print("\nTotal system pressure drops:")
-    '''
     print(f"Oxygen system: {(current_press_ox - chamber_press).to(u.psi):.2f}")
-    print(f"Ethanol system: {(current_press_eth - chamber_press).to(u.psi):.2f}")
+    print(f"Ethanol system: {(current_press_ipa - chamber_press).to(u.psi):.2f}")
     print(f"With a LOx chamber pressure of {chamber_press.to(u.psi):.2f}, the tank pressure will be {current_press_ox.to(u.psi):.2f}")
-    print(f"With an ethanol chamber pressure of {chamber_press.to(u.psi):.2f}, the tank pressure will be {current_press_eth.to(u.psi):.2f}")
-    '''
+    print(f"With an ethanol chamber pressure of {chamber_press.to(u.psi):.2f}, the tank pressure will be {current_press_ipa.to(u.psi):.2f}")
 
 if __name__ == "__main__":
     main()
