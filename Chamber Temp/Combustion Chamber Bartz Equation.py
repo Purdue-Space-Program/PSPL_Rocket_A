58f85c80d6eb22761bd6fb960ce0f1ab62509275
mukunds18 <mukunds@purdue.edu>
2025-11-04 15:06:30 -0500

    Update Combustion Chamber Bartz Equation.py

--- Chamber Temp/Combustion Chamber Bartz Equation.py
+++ Chamber Temp/Combustion Chamber Bartz Equation.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-chamber_contour_csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "ChamberContour", "chamber_contour_meters"))
+chamber_contour_csv_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "ChamberContour", "chamber_contour_meters.csv"))
 
 import constants as c
 
