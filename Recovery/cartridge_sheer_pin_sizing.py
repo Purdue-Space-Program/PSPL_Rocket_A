d6baa8938520c26e4a6850b7c8115655fc73acfc
abhigu <asidhstb@gmail.com>
2025-12-05 22:31:29 -0500

    variable name changes

--- Recovery/cartridge_sheer_pin_sizing.py
+++ Recovery/cartridge_sheer_pin_sizing.py
@@ -5,6 +5,7 @@ def calculateUpwardPressureForce(mass_g, volume2_L):
     temperature_K = 298
     pressure1_atm_volume1_L = ((moles)*(R)*(temperature_K))
     pressure2_atm = pressure1_atm_volume1_L/volume2_L
+    print(pressure2_atm)
     return (pressure2_atm*14.6959)*(3.14159*2.875*2.875)
 
 def calculateFrictionForce(force, coefficient):
@@ -19,14 +20,14 @@ def calculateNetForce(positiveForce, negativeForce):
 def main():
 
     cartridgeMass_g = 40
-    recoveryVolume = 4.2
-    upwardPressureForce = calculateUpwardPressureForce(cartridgeMass_g, recoveryVolume)
+    recoveryVolume_L = 4.2
+    upwardPressureForce_lbs = calculateUpwardPressureForce(cartridgeMass_g, recoveryVolume_L)
     
     coefficientOfFriction = 0.6
-    frictionForce = calculateFrictionForce(upwardPressureForce, coefficientOfFriction)
+    frictionForce_lbs = calculateFrictionForce(upwardPressureForce_lbs, coefficientOfFriction)
     
-    netForce = calculateNetForce(upwardPressureForce, frictionForce)
-    print(netForce)
+    netForce_lbs = calculateNetForce(upwardPressureForce_lbs, frictionForce_lbs)
+    print(netForce_lbs)
 
     #sizeSheerPins()
 
