20ca2b0a29d6afbf2818bca42428442ca4a007f1
abhigu <asidhstb@gmail.com>
2025-12-07 15:04:24 -0500

    modified force calculation

--- Recovery/cartridge_sheer_pin_sizing.py
+++ Recovery/cartridge_sheer_pin_sizing.py
@@ -1,12 +1,11 @@
-def calculateUpwardPressureForce(mass_g, deltaTemperature_K, radius_in, length_in):
+def calculateUpwardPressureForce(mass_g, deltaTemperature_K, parachuteRadius_in, radius_in, length_in):
     molesMassCO2_g = 44.01
     moles = mass_g/molesMassCO2_g
     R = 0.0821
     temperature_K = 298-deltaTemperature_K
     volume_L = (3.14159*radius_in*radius_in)*(length_in)*(0.0163871)
     pressure_atm = ((moles)*(R)*(temperature_K))/(volume_L)
-    print(pressure_atm*14.6959)
-    return (pressure_atm*14.6959)*(3.14159*radius_in*radius_in)
+    return (pressure_atm*14.6959)*(3.14159*parachuteRadius_in*parachuteRadius_in)
 
 def calculateFrictionForce(force, coefficient):
     return coefficient*force
@@ -21,9 +20,10 @@ def main():
 
     cartridgeMass_g = 26
     temperatureDrop_K = 50
+    parachuteRadius_in = 2.45
     recoveryBayRadius_in = 2.875
     recoveryBayLength_in = 10
-    upwardPressureForce_lbs = calculateUpwardPressureForce(cartridgeMass_g, temperatureDrop_K, recoveryBayRadius_in, recoveryBayLength_in)
+    upwardPressureForce_lbs = calculateUpwardPressureForce(cartridgeMass_g, temperatureDrop_K, parachuteRadius_in, recoveryBayRadius_in, recoveryBayLength_in)
     
     coefficientOfFriction = 0.6
     frictionForce_lbs = calculateFrictionForce(upwardPressureForce_lbs, coefficientOfFriction)
