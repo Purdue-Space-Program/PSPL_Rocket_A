59ac9beb139e61982eed0e600212037458097bb8
abhigu <asidhstb@gmail.com>
2025-12-07 14:56:20 -0500

    less hardcoding

--- Recovery/cartridge_sheer_pin_sizing.py
+++ Recovery/cartridge_sheer_pin_sizing.py
@@ -1,11 +1,12 @@
-def calculateUpwardPressureForce(mass_g, volume2_L):
+def calculateUpwardPressureForce(mass_g, deltaTemperature_K, radius_in, length_in):
     molesMassCO2_g = 44.01
     moles = mass_g/molesMassCO2_g
     R = 0.0821
-    temperature_K = 298
-    pressure1_atm_volume1_L = ((moles)*(R)*(temperature_K))
-    pressure2_atm = pressure1_atm_volume1_L/volume2_L
-    return (pressure2_atm*14.6959)*(3.14159*2.875*2.875)
+    temperature_K = 298-deltaTemperature_K
+    volume_L = (3.14159*radius_in*radius_in)*(length_in)*(0.0163871)
+    pressure_atm = ((moles)*(R)*(temperature_K))/(volume_L)
+    print(pressure_atm*14.6959)
+    return (pressure_atm*14.6959)*(3.14159*radius_in*radius_in)
 
 def calculateFrictionForce(force, coefficient):
     return coefficient*force
@@ -19,8 +20,10 @@ def calculateNetForce(positiveForce, negativeForce):
 def main():
 
     cartridgeMass_g = 26
-    recoveryVolume_L = 4.2
-    upwardPressureForce_lbs = calculateUpwardPressureForce(cartridgeMass_g, recoveryVolume_L)
+    temperatureDrop_K = 50
+    recoveryBayRadius_in = 2.875
+    recoveryBayLength_in = 10
+    upwardPressureForce_lbs = calculateUpwardPressureForce(cartridgeMass_g, temperatureDrop_K, recoveryBayRadius_in, recoveryBayLength_in)
     
     coefficientOfFriction = 0.6
     frictionForce_lbs = calculateFrictionForce(upwardPressureForce_lbs, coefficientOfFriction)
