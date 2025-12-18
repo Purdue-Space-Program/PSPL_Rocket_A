8d207bde838e13562a683c38b946f253b1e33844
abhigu <asidhstb@gmail.com>
2025-12-18 16:15:17 -0500

    made everything into variables

--- Recovery/cartridge_sheer_pin_sizing.py
+++ Recovery/cartridge_sheer_pin_sizing.py
@@ -1,18 +1,22 @@
 # import numpy as np
+molesMassCO2_g = 44.01
+R = 0.0821
+pi = 3.14159
+cuin_to_L = 0.0163871
+atm_to_psi = 14.6959
+
 
 def calculatePressure(mass_g, initialTemperature_K, deltaTemperature_K, radius_in, length_in):
-    molesMassCO2_g = 44.01
     moles = mass_g/molesMassCO2_g
-    R = 0.0821
     temperature_K = initialTemperature_K + deltaTemperature_K
-    volume_L = (3.14159*radius_in*radius_in)*(length_in)*(0.0163871)
-    return (((moles)*(R)*(temperature_K))/(volume_L))*14.6959
+    volume_L = (pi*radius_in*radius_in)*(length_in)*(cuin_to_L)
+    return (((moles)*(R)*(temperature_K))/(volume_L))*atm_to_psi
 
-def calculateUpwardForce(pressure_psi, radius_in, length_in):
-    return (pressure_psi)*(3.14159*radius_in*radius_in)
+def calculateUpwardForce(pressure_psi, radius_in):
+    return (pressure_psi)*(pi*radius_in*radius_in)
 
-def calculateFrictionForce(pressure_psi, radius_in, length_in, coefficient):
-    return (pressure_psi)*(3.14159*radius_in*radius_in)*coefficient
+def calculateFrictionForce(pressure_psi, radius_in, coefficient):
+    return (pressure_psi)*(pi*radius_in*radius_in)*coefficient
 
 def calculateNetForce(positiveForce, negativeForce):
     return positiveForce - negativeForce
@@ -31,11 +35,11 @@ def main():
     print(f"recoveryBayPressure_psi: {recoveryBayPressure_psi:.2f}")
     
     parachuteRadius_in = 2.45
-    upwardPressureForce_lbs = calculateUpwardForce(recoveryBayPressure_psi, parachuteRadius_in, recoveryBayLength_in)
+    upwardPressureForce_lbs = calculateUpwardForce(recoveryBayPressure_psi, parachuteRadius_in)
     print(f"upwardPressureForce_lbs: {upwardPressureForce_lbs:.2f}")
 
     coefficientOfFriction = 0.2
-    frictionForce_lbs = calculateFrictionForce(recoveryBayPressure_psi, parachuteRadius_in, recoveryBayLength_in, coefficientOfFriction)
+    frictionForce_lbs = calculateFrictionForce(recoveryBayPressure_psi, parachuteRadius_in, coefficientOfFriction)
     print(f"frictionForce_lbs: {frictionForce_lbs:.2f}")
     
     netForce_lbs = calculateNetForce(upwardPressureForce_lbs, frictionForce_lbs)
