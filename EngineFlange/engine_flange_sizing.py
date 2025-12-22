9aa95343c5753f5dd44990c91520c902898be81f
abhigu <asidhstb@gmail.com>
2025-12-22 12:53:32 -0800

    cleanup + updated FOS

--- EngineFlange/engine_flange_sizing.py
+++ EngineFlange/engine_flange_sizing.py
@@ -14,56 +14,54 @@ def lower_preload(diameter, proof):
 def bolts(force, preload):
     return force/preload
 
+def sum_forces(array, safety):
+    net = 0
+    for i in array:
+        net = net + i
+    return net * safety
+        
+
 def main ():
     proof_stress_safety_factor = 0.8
     steel_tensile_strength = 42100
     proof_stress = proof(proof_stress_safety_factor,steel_tensile_strength)
+    safety_factor = 1.4
+    #Pathfinder
+    bolt_diameter_plates = 0.25
+    bolt_diameter_pintle = 0.125
+    chamber_diameter = 4.9
+    chamber_pressure = 500
+    #CMS
+    #bolt_diameter_plates = 0.25
+    #chamber_diameter = 5.05
+    #chamber_pressure = 200
 
     #Pathfinder
-    tensile_force_from_chamber_faceplate = tensile_from_chamber(4.9,500)
+    tensile_force_from_chamber_plates = tensile_from_chamber(chamber_diameter,chamber_pressure)
     tensile_force_from_outer_o_ring = tensile_from_o_ring(5.19301,70)
     tensile_force_from_chamber_o_ring = tensile_from_o_ring(5.19302,70)
     tensile_force_from_film_o_ring = 0 #tensile_from_o_ring(3.984,70)
     tensile_force_from_manifold_o_ring = 0 #tensile_from_o_ring(3.512,70)
-    force_safety_factor_faceplate = 1.5
 
     #CMS
-    #tensile_force_from_chamber_faceplate = tensile_from_chamber(5.05,200)
+    #tensile_force_from_chamber_plates = tensile_from_chamber(chamber_diameter,chamber_pressure)
     #tensile_force_from_outer_o_ring = 0
     #tensile_force_from_chamber_o_ring = tensile_from_o_ring(5.19301,70)
     #tensile_force_from_film_o_ring = 0 
     #tensile_force_from_manifold_o_ring = 0 
-    #force_safety_factor_faceplate = 1.5
 
-    forces_faceplate = {tensile_force_from_chamber_faceplate,tensile_force_from_outer_o_ring,tensile_force_from_chamber_o_ring,tensile_force_from_film_o_ring,tensile_force_from_manifold_o_ring}
-    net_force_faceplate = sum(forces_faceplate)*force_safety_factor_faceplate
-    #print(net_force_faceplate)
-    
-    #Pathfinder
-    bolt_diameter_faceplate = 0.25
-
-    #CMS
-    #bolt_diameter_faceplate = 0.25
-
-    lower_bound_preload_faceplate=lower_preload(bolt_diameter_faceplate,proof_stress)
-    #print(lower_bound_preload_faceplate)
-
-    number_of_bolts_faceplate = bolts(net_force_faceplate,lower_bound_preload_faceplate)
-    print(number_of_bolts_faceplate)
+    forces_plates = {tensile_force_from_chamber_plates,tensile_force_from_outer_o_ring,tensile_force_from_chamber_o_ring,tensile_force_from_film_o_ring,tensile_force_from_manifold_o_ring}
+    net_force_plates = sum_forces(forces_plates, safety_factor)
+    lower_bound_preload_plates = lower_preload(bolt_diameter_plates,proof_stress)
+    number_of_bolts_plates = bolts(net_force_plates,lower_bound_preload_plates)
+    print(number_of_bolts_plates)
 
     tensile_force_from_chamber_pintle = tensile_from_chamber(.98,500)
     tensile_force_from_pintle_o_ring = tensile_from_o_ring(1.25,70)
-    force_safety_factor_pintle = 1.5
 
     forces_pintle = {tensile_force_from_chamber_pintle,tensile_force_from_pintle_o_ring}
-    net_force_pintle = sum(forces_pintle)*force_safety_factor_pintle
-    #print(net_force_pintle)
-    
-    bolt_diameter_pintle = 0.125
-
-    lower_bound_preload_pintle=lower_preload(bolt_diameter_pintle,proof_stress)
-    #print(lower_bound_preload_pintle)
-
+    net_force_pintle = sum_forces(forces_pintle, safety_factor)
+    lower_bound_preload_pintle = lower_preload(bolt_diameter_pintle,proof_stress)
     number_of_bolts_pintle = bolts(net_force_pintle,lower_bound_preload_pintle)
     print(number_of_bolts_pintle)
 
