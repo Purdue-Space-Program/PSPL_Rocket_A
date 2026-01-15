15b8dcccdc696a0f2a3e13e25d7187f6a399522b
David Z Gustafsson <david.z.gustafsson@gmail.com>
2026-01-15 13:55:55 -0500

    fuck

--- Recovery/recovery_bay_bulkhead_sizing.py
+++ Recovery/recovery_bay_bulkhead_sizing.py
@@ -9,56 +9,69 @@ import vehicle_parameters as v
 def shearStrength(material, diameter):
     return (material)*(pi*diameter*diameter*0.25)
 
-def bearingStrength(material, diameter, thickness):
-    return (material)*(diameter)*(thickness)
+def CalculateBearingStrength(material, diameter, thickness):
+    bearing_strength = material * diameter * thickness
+    return (bearing_strength)
 
-def shearUMOS(max, actual, uFOS, fFOS):
-    return (max/(actual*uFOS*fFOS)) - 1
+def CalculateMoS(maximum_allowable_load, limit_load, FOS, fitting_factor):
+    MoS = (maximum_allowable_load/(limit_load*FOS*fitting_factor)) - 1
+    return (MoS)
 
-def bearingUMOS(max, actual, uFOS, fFOS):
-    return (max/(actual*uFOS*fFOS)) - 1
+# these are not necessary to be their own functions
+# def shearUMOS(max, actual, uFOS, fitting_factor):
+#     return (max/(actual*uFOS*fitting_factor)) - 1
 
-def bearingYMOS(max, actual, yFOS, fFOS):
-    return (max/(actual*yFOS*fFOS)) - 1
+# def bearingUMOS(max, actual, uFOS, fitting_factor):
+#     return (max/(actual*uFOS*fitting_factor)) - 1
+
+# def bearingYMOS(max, actual, sUMOS, bUMOS, uFOS, yFOS, fitting_factor):
+#     if (sUMOS > bUMOS):
+#         return (max/(actual*yFOS*fitting_factor)) - 1
+#     else:
+#         return (max/(actual*uFOS*fitting_factor)) - 1
 
 def main():
+    print("Recovery Bay Connector Bolted Joint")
+    
     fSU_SS_316_U = 66000.0
     #fSU_AS_U = 180000.0
     #boltDiameter = 0.2075
     boltDiameter = 0.2614
     #boltDiameter = 0.32
     boltShearStrength = shearStrength(fSU_SS_316_U, boltDiameter)
-    print(f"boltShearStrength: {boltShearStrength}")
+    print(f"\tboltShearStrength: {boltShearStrength}")
 
-    fBRU_A_6061_U = 88000.0
-    fBRU_A_6061_Y = 58000.0
+    F_bru_A_6061_T6 = 88_000 # [psi] from MMPDS-2019: https://purdue-space-program.atlassian.net/wiki/spaces/PL/pages/1934065665/Common+Material+Properties
+    F_bry_A_6061_T6 = 58_000 # [psi] from MMPDS-2019: https://purdue-space-program.atlassian.net/wiki/spaces/PL/pages/1934065665/Common+Material+Properties
     plateThickness = 0.125
-    boltBearingStrengthUltimate = bearingStrength(fBRU_A_6061_U, boltDiameter, plateThickness)
-    boltBearingStrengthYield = bearingStrength(fBRU_A_6061_Y, boltDiameter, plateThickness)
-    print(f"boltBearingStrengthUltimate: {boltBearingStrengthUltimate}")
-    print(f"boltBearingStrengthYield: {boltBearingStrengthYield}")
+    boltBearingStrengthUltimate = CalculateBearingStrength(F_bru_A_6061_T6, boltDiameter, plateThickness)
+    boltBearingStrengthYield = CalculateBearingStrength(F_bry_A_6061_T6, boltDiameter, plateThickness)
+    print(f"\tboltBearingStrengthUltimate: {boltBearingStrengthUltimate:.2f}")
+    print(f"\tboltBearingStrengthYield: {boltBearingStrengthYield:.2f}")
 
+
+    ultimateFOS = 2.0
+    yieldFOS = 1.5
+    
     #Need to update axial force.
     #netAxialForce = 300
     netAxialForce = 16230
     #numberOfBolts = 12
     numberOfBolts = 16
     shearForcePerBolt = netAxialForce/numberOfBolts
-    ultimateFOS = 2.0
-    yieldFOS = 1.5
 
     if (boltShearStrength > boltBearingStrengthUltimate):
-        print("Bearing Critical!")
+        print("\tBearing Critical!")
         fittingFOS = 1.15
     else: 
-        print("Sheer Critical!")
+        print("\tSheer Critical!")
         fittingFOS = 2.0
 
-    shearUltimateMOS = shearUMOS(boltShearStrength, shearForcePerBolt, ultimateFOS, fittingFOS)
-    bearingUltimateMOS = bearingUMOS(boltBearingStrengthUltimate, shearForcePerBolt, ultimateFOS, fittingFOS)
-    bearingYieldMOS = bearingYMOS(boltBearingStrengthYield, shearForcePerBolt, yieldFOS, fittingFOS)
-    print(f"shearUltimateMOS: {shearUltimateMOS}")
-    print(f"bearingUltimateMOS: {bearingUltimateMOS}")
-    print(f"bearingYieldMOS: {bearingYieldMOS}")
+    shearUltimateMOS = CalculateMoS(boltShearStrength, shearForcePerBolt, ultimateFOS, fittingFOS)
+    bearingUltimateMOS = CalculateMoS(boltBearingStrengthUltimate, shearForcePerBolt, ultimateFOS, fittingFOS)
+    bearingYieldMOS = CalculateMoS(boltBearingStrengthYield, shearForcePerBolt, yieldFOS, fittingFOS)
+    print(f"\tshearUltimateMOS: {shearUltimateMOS:.2f}")
+    print(f"\tbearingUltimateMOS: {bearingUltimateMOS:.2f}")
+    print(f"\tbearingYieldMOS: {bearingYieldMOS:.2f}")
 
 main()
