3ee1bbae9e9fd25600bdf34c308ff2257d2e2a2f
David Z Gustafsson <david.z.gustafsson@gmail.com>
2026-09-21 23:08:08 -0400

    6dof record time off the rail and make it an error to edit a field that doesn't exist

--- Fluid_Systems/Tanks/pressure_calculations.py
+++ Fluid_Systems/Tanks/pressure_calculations.py
@@ -20,23 +20,23 @@ def Calculate_Pressure(parameters): # this is a separate function for reasons i 
     droop_factor = 0.67 # our guess of our regulator's droop based on bzb data
     
     parameters.unfreeze()
-    parameters.maximum_tank_pressure_to_acount_for_droop = parameters.nominal_tank_pressure / droop_factor # the set pressure is higher than the nominal tank pressure to account for droop that occurs when the run valves open and there is flow through the regulator
+    parameters.maximum_tank_pressure_to_account_for_droop = parameters.nominal_tank_pressure / droop_factor # the set pressure is higher than the nominal tank pressure to account for droop that occurs when the run valves open and there is flow through the regulator
     
-    relief_valve_set_pressure = Round_Up_To_Nearest_Multiple(original_value = (parameters.maximum_tank_pressure_to_acount_for_droop - c.ATM2PA), 
+    relief_valve_set_pressure = Round_Up_To_Nearest_Multiple(original_value = (parameters.maximum_tank_pressure_to_account_for_droop - c.ATM2PA), 
                                                              nearest_multiple = (50 * c.PSI2PA)
                                                             ) + c.ATM2PA # relief valves sold in increments and in PSIG: https://www.globaltestsupply.com/product/generant-hprva-series-vent-to-atmosphere-high-pressure-relief-valve?cfrconfigurator_id=F172453&cfrkey=HPRVA-500B-T-500&cfrmodel=500B&cfrseal=T&cfrcrack=500
         
     parameters.maximum_allowable_regulator_droop_factor = parameters.nominal_tank_pressure / relief_valve_set_pressure  # the set pressure is higher than the nominal tank pressure to account for droop that occurs when the run valves open and there is flow through the regulator
 
     
-    parameters.maximum_allowable_tank_pressure = relief_valve_set_pressure * 1.1 # relief valves are rated for full flow when they are pressurized at 10% over their set pressure
-    parameters.hydroproof_tank_pressure = parameters.maximum_allowable_tank_pressure * parameters.hydroproof_factor # this is the exact pressure to hydroproof at to prove that the tanks can handle the pressure that the relief valves will allow the tank to be
+    parameters.maximum_allowable_tank_pressure = relief_valve_set_pressure * 1.1 # relief valves are rated for full flow when they are pressurized at 10% over their set pressure, one thing i am not sure about is if it is 10% over the relief pressure in PSIA or PSIG but the difference in pressure for this in a relief valve at a set pressure of 550 is about 13.5 PSI
+    parameters.hydroproof_tank_pressure = parameters.maximum_allowable_tank_pressure * parameters.hydroproof_factor # this is the exact pressure to hydroproof at to prove that the tanks can handle the pressure that the relief valves enforce the tanks to be
     parameters.freeze()
 
     output_table_data = []
     
     output_table_data.append(("nominal_tank_pressure", f"{parameters.nominal_tank_pressure * c.PA2PSI:.2f} PSIA"))
-    output_table_data.append(("maximum_tank_pressure_to_acount_for_droop", f"{parameters.maximum_tank_pressure_to_acount_for_droop * c.PA2PSI:.2f} PSIA"))
+    output_table_data.append(("maximum_tank_pressure_to_account_for_droop", f"{parameters.maximum_tank_pressure_to_account_for_droop * c.PA2PSI:.2f} PSIA"))
     output_table_data.append(("relief_valve_set_pressure", f"{relief_valve_set_pressure * c.PA2PSI:.2f} PSIA"))
     output_table_data.append(("relief_valve_set_pressure", f"{(relief_valve_set_pressure - c.ATM2PA) * c.PA2PSI:.2f} PSIG"))
     output_table_data.append(("maximum_allowable_regulator_droop_factor", f"{parameters.maximum_allowable_regulator_droop_factor * 100:.2f}%"))
